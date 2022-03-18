cdc30a0e5aa1d6c6affcf45da3c7849761df6d18
parkerjustice <you@example.com>
2022-03-18 16:32:31 -0400

    added sql

---
+++ seed.sql
@@ -0,0 +1,18 @@
+USE yes;
+
+INSERT INTO PRODUCTION (Departments)
+VALUES  
+("Camera"),("IDEAS"), ("HR"),("TALENT")
+INSERT INTO TEAMMATES ("First and Last")
+VALUES
+("Angela", "Martin"),
+("Michael", "Scott"),
+("Jim", "Halpert")
+
+
+INSERT INTO ROLE ("ROLES")
+VALUES
+("Camera MAn"),
+("HR"),
+("Talent"),
+("Creative")
