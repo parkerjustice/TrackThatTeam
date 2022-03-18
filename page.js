047857c4573eed54fe95d6bace6034089439b05a
parkerjustice <you@example.com>
2022-03-18 15:23:42 -0400

    Added JS

--- page.js
+++ page.js
@@ -7,11 +7,23 @@ const allRoles = {
     all: "show every role available",
     add: "add a new role",
     delete: "delete a role",
+    initialPage: "going back to homepage"
 }
 
-const yourStartingPoint = mysql.createConnection (
+const initialPage = {
+Production: "all the roles",
+TeamMates: "these are the employees",
+Role: "the specific roles"
 
-    {
+}
 
-    }
-)
+//functions 
+
+function mainPage() {
+    console.log('test')
+    inquirer.prompt({
+        type: "list", message: "pick one",
+        choices: [initialPage.Role, initialPage.TeamMates, initialPage.Production]
+        
+    })
+}
