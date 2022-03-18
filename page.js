27ebf344c738050ec0cc2a18a8e3567266b36730
parkerjustice <you@example.com>
2022-03-18 15:09:55 -0400

    added js

---
+++ page.js
@@ -0,0 +1,12 @@
+// These are your dependencies//
+const sql = require("mysql")
+const inquirer = require("inquirer")
+const consoleTable = require("console.table");
+
+
+const startingPage = {
+    Department:"Department",
+    Roles: "Roles",
+    Team: "Team",
+
+}
